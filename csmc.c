bc35fd65c409c23d48c145d470c3cb844c5ce263
Anthea Abreo <anthea.abreo@gmail.com>
2022-11-21 17:49:02 -0600

    Modify csmc.c

--- csmc.c
+++ csmc.c
@@ -2,6 +2,37 @@
  * ANTHEA ABREO - AXA210122, HIMA SAI KIRAN PRUDHIVI - HXP220011
  */
 
-pthread_t coordinator;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+typedef struct student{
+   int student_ID;
+   int num_helps;
+   sem_t student_sleeping;
+}student;
 
 
+int main(int argc, char *argv[]){
+    if(argc == 4){
+        int n = atoi(argv[0]);
+        int m = atoi(argv[1]);
+        int total_chairs = atoi(argv[2]);
+        int max_help = atoi(argv[3]);
+
+        student* students = (student*) malloc(n * sizeof(student));
+    }
+
+    else{
+        printf("Wrong number of arguments");
+        exit(0);
+    }
+
+    return 0;
+}
