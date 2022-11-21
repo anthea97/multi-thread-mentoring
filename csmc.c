06e4bfa483f92700c1885af106df34e88863e545
Anthea Abreo <anthea.abreo@gmail.com>
2022-11-21 17:20:19 -0600

    Modify csmc.c

--- csmc.c
+++ csmc.c
@@ -1,14 +1,7 @@
-/*
- * Command Line Arguments:
- * #students = n
- * #Tutors = m
- * #chairs = chairs_avail
- * #helps = help_max
+/* Multi-Threaded Mentoring Center
+ * ANTHEA ABREO - AXA210122, HIMA SAI KIRAN PRUDHIVI - HXP220011
  */
 
 pthread_t coordinator;
 
-// take input from user
-
-pthread_t student[n], tutor[m];
 
