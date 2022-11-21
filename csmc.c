d5a82ee695d3697767d2aa586a52118214f7b9f1
Anthea Abreo <anthea.abreo@gmail.com>
2022-11-21 11:33:22 -0600

    Modify readme , add csmc.c

---
+++ csmc.c
@@ -0,0 +1,14 @@
+/*
+ * Command Line Arguments:
+ * #students = n
+ * #Tutors = m
+ * #chairs = chairs_avail
+ * #helps = help_max
+ */
+
+pthread_t coordinator;
+
+// take input from user
+
+pthread_t student[n], tutor[m];
+
